6e0be92096820c497c7511b1d3d93bdbf18c7a5b
Dmitry <demondima123456@gmail.com>
2013-12-08 20:39:39 +0600

    initial commit

---
+++ SLUT-Tests/ExtensionsTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SLUT;
+
+namespace SLUT_Tests
+{
+    [TestClass]
+    public class ExtensionsTests
+    {
+        [TestMethod]
+        public void ForEachTest()
+        {
+            var seq = new byte[] { 0x13, 0xA1, 0xFF };
+
+            int acc = 0;
+            seq.ForEach(n => acc+=n);
+
+            Assert.AreEqual(0x13+ 0xA1+ 0xFF, acc);
+
+        }
+    }
+}
---
+++ Utilits/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLUT
+{
+    public static class Extensions
+    {
+        public static void ForEach<T>(this IEnumerable<T> source,Action<T> func)
+        {
+            foreach (T item in source)
+            {
+                func(item);
+            }
+        }
+    }
+}
