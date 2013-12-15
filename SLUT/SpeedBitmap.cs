8e863a5da9564d390cff70bdc6d33b0be024fe9c
Dmitry <demondima123456@gmail.com>
2013-12-16 00:03:58 +0600

    [SpeedBitmap.cs] add indexer and new method for lock bitmap

--- SLUT/SpeedBitmap.cs
+++ SLUT/SpeedBitmap.cs
@@ -37,16 +37,16 @@ namespace SLUT
             img_data = img.LockBits(new Rectangle(Point.Empty, img.Size), 
                 ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
 
-            // Инициализируем массив для пикселей img
-            image_pixel_array = new byte[img_data.Height * img_data.Stride];
-
-            // Заполняем массив
-            Marshal.Copy(
-                source: img_data.Scan0,
-                destination: image_pixel_array, 
-                startIndex: 0, 
-                length: image_pixel_array.Length);
+            copyPixelsToArray();
         }
+        public void LockBitmap(Rectangle rectangle)
+        {
+            img_data = img.LockBits(
+                rectangle, ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
+
+            copyPixelsToArray();
+        }
+
         public void UnlockBitmap()
         {
             // Копируем измененные данные обратно в изображение
@@ -57,6 +57,19 @@ namespace SLUT
                 length: image_pixel_array.Length);
 
             img.UnlockBits(img_data);
+        }
+
+        private void copyPixelsToArray()
+        {
+            // Инициализируем массив для пикселей img
+            image_pixel_array = new byte[img_data.Height * img_data.Stride];
+
+            // Заполняем массив
+            Marshal.Copy(
+                source: img_data.Scan0,
+                destination: image_pixel_array,
+                startIndex: 0,
+                length: image_pixel_array.Length);
         }
 
         public Color GetPixel(int x, int y)
@@ -81,6 +94,12 @@ namespace SLUT
             image_pixel_array[start_index + 1] = c.G;
             image_pixel_array[start_index + 2] = c.R;
             image_pixel_array[start_index + 3] = c.A;
+        }
+
+        public Color this[int x, int y]
+        {
+            get { return GetPixel(x, y); }
+            set { SetPixel(x, y, value); }
         }
     }
 }
