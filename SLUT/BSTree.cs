9571373a9fefc081bd7c6142c7c022cbc588a32f
Dmitry <demondima123456@gmail.com>
2013-12-21 17:41:54 +0600

    [BSTree.cs] add remove method

--- SLUT/BSTree.cs
+++ SLUT/BSTree.cs
@@ -11,16 +11,41 @@ namespace HRUC
         T Data { get; set; }
         IBinaryTree<T> LeftNode { get; set; }
         IBinaryTree<T> RightNode { get; set; }
-        bool IsEmpty { get; }
+        bool IsLeaf { get; }
     }
 
     public class BSTree<TKey,TValue> : IBinaryTree<TValue>
         where TKey: IComparable<TKey>
     {
+        #region public propertys
         public TKey Key { get; set; }
         public TValue Data { get; set; }
-        public BSTree<TKey, TValue> LeftNode { get; set; }
-        public BSTree<TKey, TValue> RightNode { get; set; }
+        public BSTree<TKey, TValue> LeftNode {
+            get { return left_node; }
+            set
+            {
+                left_node = value;
+                if (value != null) left_node.parent = this;
+            }
+        }
+        public BSTree<TKey, TValue> RightNode
+        {
+            get
+            { return right_node; }
+            set
+            {
+                right_node = value;
+                if (value != null) right_node.parent = this;
+            }
+        }
+        #endregion
+        #region fields
+        // открытого parent не будет!
+        private BSTree<TKey, TValue> parent;
+        private BSTree<TKey, TValue> left_node;
+        private BSTree<TKey, TValue> right_node;
+        #endregion
+
 
         public BSTree<TKey, TValue> Find(TKey k)
         {
@@ -33,13 +58,13 @@ namespace HRUC
             {
                 if (RightNode != null)
                     return RightNode.Find(k);
-                else throw new InvalidOperationException("Элемента нету! уперли!");
+                else throw new InvalidOperationException("Элемента нету!");
             }
             else 
             {
                 if (LeftNode != null)
                     return LeftNode.Find(k);
-                else throw new InvalidOperationException("Элемента нету! уперли!");
+                else throw new InvalidOperationException("Элемента нету!");
             }
         }
         public void Insert(TKey k, TValue data)
@@ -56,14 +81,61 @@ namespace HRUC
                     LeftNode.Insert(k, data); // рекурсивный поиск
                 else
                     LeftNode = new BSTree<TKey, TValue>() // создание ветви 
-                    { Key = k, Data = data };
+                    { Key = k, Data = data, parent = this };
             }
             else
             {
                 if (RightNode != null)
                     RightNode.Insert(k, data);
                 else
-                    RightNode = new BSTree<TKey, TValue>() { Key = k, Data = data };
+                    RightNode = new BSTree<TKey, TValue>() 
+                    { Key = k, Data = data, parent = this };
+            }
+        }
+
+        public void Remove(TKey k)
+        {
+            var del_node = Find(k);
+            var del_parent = del_node.parent;
+
+            if (del_node.IsLeaf)
+            {
+                // Удаляем del_node
+
+                if (del_parent.Key.CompareTo(del_node.Key) > 0)
+                    del_parent.LeftNode = null;
+                else del_parent.RightNode = null;
+
+                // Необходимо?
+                // del_node.parent = null;
+                // сборщик мусора и так очистит
+            }
+            // есть только одна дочерняя нода
+            else if (del_node.LeftNode == null ^ del_node.RightNode == null)
+            {
+                // Вставляем дочернюю на место удаляемой
+                if (del_parent.Key.CompareTo(del_node.Key) > 0)
+                {
+                    del_parent.LeftNode = del_node.LeftNode ?? del_node.RightNode;
+                }
+                else
+                {
+                    del_parent.LeftNode = del_node.LeftNode ?? del_node.RightNode;
+                }
+            }
+            // обе дочерние ветви существуют
+            else
+            {
+                // если есть правый потомок левого дерева
+                if (del_node.RightNode.LeftNode != null)
+                {
+                    // обменять значение и ключь
+                    del_node.Data = del_node.RightNode.LeftNode.Data;
+                    del_node.Key = del_node.RightNode.LeftNode.Key;
+
+                    // рекурсивно удалить потомка
+                    del_node.RightNode.LeftNode.Remove(del_node.RightNode.LeftNode.Key);
+                }
             }
         }
 
@@ -78,7 +150,7 @@ namespace HRUC
             get { return RightNode; }
             set { RightNode = (BSTree<TKey,TValue>)value; }
         }
-        bool IBinaryTree<TValue>.IsEmpty
+        public bool IsLeaf
         {
             get { return LeftNode == null || RightNode == null; }
         }
@@ -99,15 +171,17 @@ namespace HRUC
                 return true; // выход из рекурсии
 
             if (bstree.LeftNode == null && bstree.RightNode != null)
-                return compareWithRightTree() && IsCorrectBSTree(bstree.RightNode);
+                return compareWithRightTree() // правый ключь больше конрня
+                    && IsCorrectBSTree(bstree.RightNode); // рекурсивно для правой ветви
 
             if (bstree.RightNode == null && bstree.LeftNode != null)
-                return compareWithLeftTree() && IsCorrectBSTree(bstree.LeftNode);
+                return compareWithLeftTree() // левый ключь меньше корня
+                    && IsCorrectBSTree(bstree.LeftNode); // рекурсивно для левой ветви
 
-            return compareWithLeftTree() // Верно левая ветвь
-                && compareWithRightTree() // Правая ветвь
-                && IsCorrectBSTree(bstree.LeftNode) // Левое дерево
-                && IsCorrectBSTree(bstree.RightNode); // Правое дерево
+            return compareWithLeftTree() // левый ключь меньше корня
+                && compareWithRightTree() // правый ключь больше корня
+                && IsCorrectBSTree(bstree.LeftNode) // рекурсивный вызов для левой ветви
+                && IsCorrectBSTree(bstree.RightNode); // .. для правой ветви
 
         }
     }
