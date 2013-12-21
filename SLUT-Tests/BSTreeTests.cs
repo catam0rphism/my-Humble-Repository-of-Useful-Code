dcf3056c95f13743c28729e1118c9c918d5fe6f4
Dmitry <demondima123456@gmail.com>
2013-12-21 21:35:16 +0600

    [BSTree.cs] add tests

--- SLUT-Tests/BSTreeTests.cs
+++ SLUT-Tests/BSTreeTests.cs
@@ -89,5 +89,102 @@ namespace HRUC_Tests
             Assert.AreEqual("4", tree.LeftNode.Data);
             Assert.AreEqual("5", tree.LeftNode.RightNode.Data);
         }
+
+        [TestMethod]
+        public void  Infix_traverse_tree()
+        {
+            BSTree<int, string> tree = new BSTree<int, string>()
+            {
+                Data = "7",
+                Key = 7
+            };
+
+            tree.Insert(3, "3");
+            tree.Insert(8, "8");
+            tree.Insert(2, "2");
+            tree.Insert(5, "5");
+            tree.Insert(9, "9");
+            tree.Insert(4, "4");
+
+            ///     7
+            ///    / \
+            ///   3   8
+            ///  / \   \
+            /// 2   5   9
+            ///    /
+            ///   4
+
+            string acc = "";
+            Action<int, string> add_to_acc = (_, el) => acc += " " + el;
+            // Как победить вывод первого пробела! (логические флаги только захломляют код)
+
+            tree.Traverse_infix(add_to_acc);
+
+            Assert.AreEqual(" 2 3 4 5 7 8 9", acc);
+        }
+
+        [TestMethod]
+        public void Postfix_traverse_tree()
+        {
+            BSTree<int, string> tree = new BSTree<int, string>()
+            {
+                Data = "7",
+                Key = 7
+            };
+
+            tree.Insert(3, "3");
+            tree.Insert(8, "8");
+            tree.Insert(2, "2");
+            tree.Insert(5, "5");
+            tree.Insert(9, "9");
+            tree.Insert(4, "4");
+
+            ///     7
+            ///    / \
+            ///   3   8
+            ///  / \   \
+            /// 2   5   9
+            ///    /
+            ///   4
+
+            string acc = "";
+            Action<int, string> add_to_acc = (_, el) => acc += " " + el;
+
+            tree.Traverse_postfix(add_to_acc);
+
+            Assert.AreEqual(" 2 4 5 3 9 8 7", acc);
+        }
+
+        [TestMethod]
+        public void Prefix_traverse_tree()
+        {
+            BSTree<int, string> tree = new BSTree<int, string>()
+            {
+                Data = "7",
+                Key = 7
+            };
+
+            tree.Insert(3, "3");
+            tree.Insert(8, "8");
+            tree.Insert(2, "2");
+            tree.Insert(5, "5");
+            tree.Insert(9, "9");
+            tree.Insert(4, "4");
+
+            ///     7
+            ///    / \
+            ///   3   8
+            ///  / \   \
+            /// 2   5   9
+            ///    /
+            ///   4
+
+            string acc = "";
+            Action<int, string> add_to_acc = (_, el) => acc += " " + el;
+
+            tree.Traverse_prefix(add_to_acc);
+
+            Assert.AreEqual(" 7 3 2 5 4 8 9", acc);
+        }
     }
 }
