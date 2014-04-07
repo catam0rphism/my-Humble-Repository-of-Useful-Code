1a346a069cba4af53c0a0e3f5c234fb49d6dbd3b
Dmitry <demondima123456@gmail.com>
2014-04-07 17:07:57 +0600

    add InputBox component
    InputBox is a component for inputting some data (string, int, double)

---
+++ HRUC/Components/InputBox.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace HRUC.Components
+{
+    partial class InputBox
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox.Location = new System.Drawing.Point(155, 0);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(111, 20);
+            this.textBox.TabIndex = 0;
+            // 
+            // label
+            // 
+            this.label.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(0, 3);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(89, 13);
+            this.label.TabIndex = 1;
+            this.label.Text = "label1bbbbbbbbb";
+            // 
+            // InputBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.textBox);
+            this.Name = "InputBox";
+            this.Size = new System.Drawing.Size(267, 20);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Label label;
+    }
+}
---
+++ HRUC/Components/InputBox.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HRUC.Components
+{
+    public partial class InputBox : UserControl
+    {
+        public InputBox()
+        {
+            InitializeComponent();
+            //Resize += (s, ea) => setTextBoxSizeAndLocation();
+            //label.TextChanged += (s, ea) => setTextBoxSizeAndLocation();
+
+        }
+
+        private void setTextBoxSizeAndLocation()
+        {
+            const int padding = 3;
+            textBox.Width = this.Width - label.Width - padding;
+            textBox.Location = new Point(label.Width + padding, 0);
+        }
+        public int TextBoxWidth
+        {
+            get { return textBox.Width; }
+            set
+            {
+                textBox.Width = value;
+                textBox.Location = new Point(this.Width - value, 0);
+            }
+        }
+        public string LabelText
+        {
+            get { return label.Text; }
+            set
+            {
+                label.Text = value;
+                setTextBoxSizeAndLocation();
+            }
+        }
+        public string ValueString
+        {
+            get { return textBox.Text;}
+            set { textBox.Text = value; }
+        }
+        public int? ValueInt
+        {
+            get
+            {
+                int i;
+                if (int.TryParse(textBox.Text, out i))
+                {
+                    return i;
+                }
+                else { return null; }
+            }
+            set
+            {
+                if (value.HasValue)
+                {
+                    textBox.Text = value.Value.ToString();
+                }
+                else { textBox.Text = ""; }
+            }
+        }
+        public double ValueDouble
+        {
+            get
+            {
+                double i;
+                if (double.TryParse(textBox.Text, out i))
+                {
+                    return i;
+                }
+                else { return double.NaN; }
+            }
+            set
+            {
+                textBox.Text = value.ToString();
+            }
+        }
+    }
+}
